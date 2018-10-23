38efd3311ed5d5fdf84ebd3711583e125ed3fd0d
hasan <hasanovic.hasan@outlook.com>
2018-10-23 12:47:38 +0200

    test

--- app/src/main/java/com/example/group8/dindrikkelek/Lek.java
+++ app/src/main/java/com/example/group8/dindrikkelek/Lek.java
@@ -1,7 +1,40 @@
 package com.example.group8.dindrikkelek;
 
 public class Lek {
+    private int idLek_PK;
+    private String lekNavn;
+    private String utfall_FK;
+    private String beskrivelse;
 
-// Heisann test
+   // public Lek() {}
+    public Lek (int id, String leknavn, String beskrivelsen) {
+        this.idLek_PK = id;
+        this.lekNavn = leknavn;
+        this.beskrivelse = beskrivelsen;
+    }
+
+    public int getIdLek_PK() {
+        return idLek_PK;
+    }
+
+    public void setIdLek_PK(int idLek_PK) {
+        this.idLek_PK = idLek_PK;
+    }
+
+    public String getLekNavn() {
+        return lekNavn;
+    }
+
+    public void setLekNavn(String lekNavn) {
+        this.lekNavn = lekNavn;
+    }
+
+    public String getBeskrivelse() {
+        return beskrivelse;
+    }
+
+    public void setBeskrivelse(String beskrivelse) {
+        this.beskrivelse = beskrivelse;
+    }
 
 }
