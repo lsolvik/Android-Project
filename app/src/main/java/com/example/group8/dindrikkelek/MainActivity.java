f7617758c73aa1deab837bcd9474ec2bc0bea660
hasan <hasanovic.hasan@outlook.com>
2018-10-23 12:47:38 +0200

    test

--- app/src/main/java/com/example/group8/dindrikkelek/MainActivity.java
+++ app/src/main/java/com/example/group8/dindrikkelek/MainActivity.java
@@ -5,9 +5,14 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    dbHandler db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        db = new dbHandler (this);
+
     }
 }
