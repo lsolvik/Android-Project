f06c6aa8f9e1812255b2f5ae6dd32d861219be8e
hasan <hasanovic.hasan@outlook.com>
2018-10-24 15:40:37 +0200

    24.10.2018 15:40

--- app/src/main/java/com/example/group8/dindrikkelek/MainActivity.java
+++ app/src/main/java/com/example/group8/dindrikkelek/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.group8.dindrikkelek;
 
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
@@ -18,6 +19,20 @@ public class MainActivity extends AppCompatActivity {
         try {
             SQLiteDatabase db = dbhandler.getWritableDatabase();
             //Code to read/write data from/to the database
+            Cursor cursor = db.query ("LEK",
+                    new String[] {"idLEK_PK", "LEKNAVN", "BESKRIVELSE"},
+                    null, null, null, null, null);
+
+            if (cursor != null && cursor.moveToFirst()) {
+                String navn = cursor.getString(1);
+                String beskrivelse = cursor.getString(2);
+                Toast t = Toast.makeText(this, navn + beskrivelse, Toast.LENGTH_LONG);
+
+                t.show();
+                cursor.close();
+                db.close();
+            }
+
         } catch (SQLiteException e) {
             Toast toast = Toast.makeText(this, "Database ikke tilgjengelig.", Toast.LENGTH_SHORT);
             toast.show();
