8bae3724f4a3bdbc91a4a2de41f570e56e53ccfb
leona <leonardsl1@hotmail.com>
2018-11-22 19:50:33 +0100

    Spilloppdatering. Henter nå ut tilfeldig lek, og tilfeldig twist ut ifra ifra leken

--- app/src/main/java/com/example/group8/dindrikkelek/dbHandler.java
+++ app/src/main/java/com/example/group8/dindrikkelek/dbHandler.java
@@ -44,6 +44,12 @@ public class dbHandler extends SQLiteOpenHelper {
         insertLek(db, "Shots", "Shots fungerer slik....");
         insertLek(db, "Jeg har aldri", "Jeg har aldri fungerer slik....");
         insertLek(db, "Personen som", "Personen som fungerer slik....");
+        insertUtfall(db, "Må ta tre shots", 2);
+        insertUtfall(db, "Pek på personen med størst nese", 1);
+        insertUtfall(db, "Jeg har aldri brent opp barbidukkene mine", 3);
+        insertUtfall(db, "Personen som søler neste gang må spandere drinker", 4);
+        insertUtfall(db, "Jeg har aldri spist en edderkopp", 3);
+        insertUtfall(db, "Pek på den som er mest drita", 1);
 
 
     }
@@ -55,6 +61,14 @@ public class dbHandler extends SQLiteOpenHelper {
         lekValues.put("BESKRIVELSE", beskrivelse);
         db.insert("LEK", null, lekValues);
     }
+
+    private static void insertUtfall (SQLiteDatabase db, String Utfalltekst, int IdLEK_FK){
+        ContentValues utfallValues = new ContentValues();
+        utfallValues.put("UTFALLTEKST", Utfalltekst);
+        utfallValues.put("idLEK_FK", IdLEK_FK);
+        db.insert("Utfall", null, utfallValues);
+    }
+
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
@@ -91,6 +105,7 @@ public class dbHandler extends SQLiteOpenHelper {
     public List<String> getAllLeker(){
         List<String> leker = new ArrayList<>();
 
+
         String query = "SELECT * FROM LEK";
         SQLiteDatabase db = this.getReadableDatabase();
         Cursor cursor = db.rawQuery(query, null);
@@ -102,9 +117,130 @@ public class dbHandler extends SQLiteOpenHelper {
             } while (cursor.moveToNext());
         }
 
+
+
         cursor.close();
         db.close();
         return leker;
+
+    }
+
+    List<String> lekerFK = new ArrayList<>();
+
+
+
+    public List<String> getLekerFK(){
+        List<String> lekerFK2 = lekerFK;
+        return lekerFK2;
+
+    }
+
+    //returnerer liste av leker FK
+    public List<String> setLekerFK(String randomlekID){
+
+
+       lekerFK.clear();
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.query("LEK",
+                new String[] {"idLEK_PK", "LEKNAVN"},
+                "idLEK_PK = ?",
+                new String[]{(randomlekID)}, null, null, null);
+
+        //looper gjennom alle rader og legger dem til i lista
+        if (cursor.moveToFirst()) {
+            String lekPK = cursor.getString(0);
+            String leknavn = cursor.getString(1);
+            lekerFK.add(lekPK);
+            lekerFK.add(leknavn);
+
+        }
+
+
+        cursor.close();
+        db.close();
+        return lekerFK;
+
+    }
+
+
+
+    public List<String> getRandomLekPK(){
+        List<String> leker = new ArrayList<>();
+
+
+        String query = "SELECT * FROM LEK";
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(query, null);
+
+        //looper gjennom alle rader og legger dem til i lista
+        if (cursor.moveToFirst()) {
+            do {
+                leker.add(cursor.getString(0));
+            } while (cursor.moveToNext());
+        }
+
+
+
+        cursor.close();
+        db.close();
+        return leker;
+
+    }
+
+/*
+
+    List<String> lekFK = new ArrayList<>();
+
+    public List<String> setLekFK(String randomLek){
+        String query = ("SELECT * FROM LEK WHERE LEKNAVN = ?");
+        String [] args = new String[] {randomLek};
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(query, args);
+        if (cursor.moveToFirst()) {
+            do {
+                lekFK.add(cursor.getString(0));
+            } while (cursor.moveToNext());
+        }
+        return lekFK;
+
+
+    }
+
+
+    public List<String> getLekFK() {
+        //Utfall.add("heisann");
+        List<String> lekFK2 = new ArrayList<>(lekFK);
+        return lekFK2;
+    }
+
+*/
+    List<String> Utfall = new ArrayList<>();
+
+    public List<String> getUTfalltekst() {
+        //Utfall.add("heisann");
+        List<String> Utfall2 = new ArrayList<>(Utfall);
+        return Utfall2;
+    }
+
+
+
+    public List<String> setUtfallPK(String randomlek) {
+        Utfall.clear();
+        String query = ("SELECT * FROM Utfall WHERE idLEK_fk= ?");
+        String[] arguments= new String[]{randomlek};
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(query, arguments);
+        String strengen;
+        if (cursor.moveToFirst()) {
+            do {
+                Utfall.add(cursor.getString(1));
+            } while (cursor.moveToNext());
+        }
+
+        cursor.close();
+        db.close();
+        return Utfall;
+
     }
 
     //returnerer data fra LEK og UTFALL
@@ -115,6 +251,14 @@ public class dbHandler extends SQLiteOpenHelper {
                 "WHERE idLEK_PK = idLEK_FK;";
         Cursor data = db.rawQuery(query, null);
         return data;
+    }
+
+    public void dropUtfall(){
+        SQLiteDatabase db = getWritableDatabase();
+        db.execSQL("DROP TABLE IF EXISTS LEK");
+        db.execSQL("DROP TABLE IF EXISTS BILDE");
+        db.execSQL("DROP TABLE IF EXISTS UTFALL");
+        onCreate(db);
     }
 
 }
